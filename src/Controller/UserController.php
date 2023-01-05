e4bc6a0302b5c85eeb91d2a95f29e87d0fe5087c
zych.johann@gmail.com <zych.johann@gmail.com>
2023-01-05 17:18:30 +0100

    Add user by Admin

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -8,6 +8,7 @@ use App\Form\UserType;
 use App\Repository\IdeaRepository;
 use App\Repository\ProjectRepository;
 use App\Repository\UserRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -25,13 +26,15 @@ class UserController extends AbstractController
     }
 
     #[Route('/new', name: 'app_user_new', methods: ['GET', 'POST'])]
+    #[IsGranted('ROLE_ADMIN')]
     public function new(Request $request, UserRepository $userRepository): Response
     {
         $user = new User();
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
-
         if ($form->isSubmitted() && $form->isValid()) {
+            $passwordUser = $user->getPassword();
+            $user->setPassword(password_hash($passwordUser, PASSWORD_DEFAULT));
             $userRepository->save($user, true);
             return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
         }
@@ -39,6 +42,7 @@ class UserController extends AbstractController
         return $this->renderForm('user/new.html.twig', [
             'user' => $user,
             'form' => $form,
+            'edit' => true,
         ]);
     }
 
@@ -76,8 +80,9 @@ class UserController extends AbstractController
     {
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
-
         if ($form->isSubmitted() && $form->isValid()) {
+            $passwordUser = $user->getPassword();
+            $user->setPassword(password_hash($passwordUser, PASSWORD_DEFAULT));
             $userRepository->save($user, true);
 
             return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
@@ -86,6 +91,7 @@ class UserController extends AbstractController
         return $this->renderForm('user/edit.html.twig', [
             'user' => $user,
             'form' => $form,
+            'edit' => true,
         ]);
     }
 
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -14,10 +15,10 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('firstname', TextType::class, ['disabled' => true])
-            ->add('lastname', TextType::class, ['disabled' => true])
-            ->add('email', TextType::class, ['disabled' => true])
-//            ->add('password')
+            ->add('firstname', TextType::class)
+            ->add('lastname', TextType::class)
+            ->add('email', TextType::class)
+            ->add('password', PasswordType::class)
 //            ->add('role')
             ->add('avatar')
             ->add('birthday', DateType::Class, array(
