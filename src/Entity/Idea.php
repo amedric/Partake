bed887b4f2dd9f848deca6d878055680c3c09f36
ToloCrook <tolotraramaro@hotmail.fr>
2023-01-09 11:41:52 +0100

    pushing entities modification only

--- src/Entity/Idea.php
+++ src/Entity/Idea.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\IdeaRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -34,6 +36,14 @@ class Idea
 
     #[ORM\ManyToOne]
     private ?Project $project = null;
+
+    #[ORM\OneToMany(mappedBy: 'idea', targetEntity: Comment::class)]
+    private Collection $comments;
+
+    public function __construct()
+    {
+        $this->comments = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -120,6 +130,36 @@ class Idea
     public function setProject(?Project $project): self
     {
         $this->project = $project;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Comment>
+     */
+    public function getComments(): Collection
+    {
+        return $this->comments;
+    }
+
+    public function addComment(Comment $comment): self
+    {
+        if (!$this->comments->contains($comment)) {
+            $this->comments->add($comment);
+            $comment->setIdea($this);
+        }
+
+        return $this;
+    }
+
+    public function removeComment(Comment $comment): self
+    {
+        if ($this->comments->removeElement($comment)) {
+            // set the owning side to null (unless already changed)
+            if ($comment->getIdea() === $this) {
+                $comment->setIdea(null);
+            }
+        }
 
         return $this;
     }
