1616e668accab84982a61e56e16b63e7fbe253b4
brignoli-pitt <pierre.brignoli@gmail.com>
2023-01-17 11:29:11 +0100

    add form select and getFullName method

--- src/Form/Project1Type.php
+++ src/Form/Project1Type.php
@@ -3,6 +3,8 @@
 namespace App\Form;
 
 use App\Entity\Project;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -21,6 +23,17 @@ class Project1Type extends AbstractType
             ->add('projectViews')
             ->add('projectColor')
             ->add('category', null, ['choice_label' => 'title'])
+            ->add('usersSelectedOnProject', EntityType::class, [
+                'class' => User::class,
+                'required' => true,
+                'multiple' => true,
+                'choice_label' => function (User $user) {
+                    return $user->getFullName();
+                },
+                'group_by' => function (User $user) {
+                    return $user->getTeam();
+                }
+            ])
         ;
     }
 
