25403d9f59c32efa4785f64a96d557d17c0a565b
amedric <andyricana@hotmail.com>
2023-01-18 17:14:53 +0100

    push branch

--- assets/js/horizontalScroll.js
+++ assets/js/horizontalScroll.js
@@ -26,3 +26,32 @@ function sideScroll(element,direction,speed,distance,step){
         }
     }, speed);
 }
+
+// ----------------------- Draggable cards container --------------------------------
+const slider = document.querySelector('.ptk-cardsContainer');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', (e) => {
+    isDown = true;
+    slider.classList.add('active');
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+});
+slider.addEventListener('mouseleave', () => {
+    isDown = false;
+    slider.classList.remove('active');
+});
+slider.addEventListener('mouseup', () => {
+    isDown = false;
+    slider.classList.remove('active');
+});
+slider.addEventListener('mousemove', (e) => {
+    if(!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    const walk = (x - startX) * 3; //scroll-fast
+    slider.scrollLeft = scrollLeft - walk;
+    console.log(walk);
+});
