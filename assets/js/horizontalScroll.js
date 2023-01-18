c803be3c9111e8983173de10009d961de94493c0
amedric <andyricana@hotmail.com>
2023-01-18 16:30:21 +0100

    commit changes

--- assets/js/horizontalScroll.js
+++ assets/js/horizontalScroll.js
@@ -1,10 +1,28 @@
-const rightButton = document.getElementById('ptk-rightBttnId');
-const leftButton = document.getElementById('ptk-leftBttnId');
-
-rightButton.onclick = () => {
-    document.getElementById('ptk-cardsContainerId').scrollLeft += 230;
+// ------------------ functions for scrolling left and right --------------------------------
+var rightButton = document.getElementById('ptk-rightBttnId');
+rightButton.onclick = function () {
+    var container = document.getElementById('ptk-cardsContainerId');
+    sideScroll(container,'right',10,280,10);
 };
 
-leftButton.onclick = () => {
-    document.getElementById('ptk-cardsContainerId').scrollLeft -= 230;
+var leftButton = document.getElementById('ptk-leftBttnId');
+leftButton.onclick = function () {
+    var container = document.getElementById('ptk-cardsContainerId');
+    sideScroll(container,'left',10,280,10);
 };
+
+// -------------------Smooth scrolling --------------------------------
+function sideScroll(element,direction,speed,distance,step){
+    scrollAmount = 0;
+    var slideTimer = setInterval(function(){
+        if(direction == 'left'){
+            element.scrollLeft -= step;
+        } else {
+            element.scrollLeft += step;
+        }
+        scrollAmount += step;
+        if(scrollAmount >= distance){
+            window.clearInterval(slideTimer);
+        }
+    }, speed);
+}
