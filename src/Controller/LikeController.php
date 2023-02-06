08ef6bace2ad56c5d4e9abed5fd343c52f3292ab
zych.johann@gmail.com <zych.johann@gmail.com>
2023-02-06 15:47:44 +0100

    fix delete Project and Like/dislike in comment

--- src/Controller/LikeController.php
+++ src/Controller/LikeController.php
@@ -11,7 +11,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class LikeController extends AbstractController
 {
-    #[Route('/idea/{id}/like', name: 'app_like_like', methods: ['GET', 'POST'])]
+    #[Route('//idea/{id}/like', name: 'app_like_like', methods: ['GET', 'POST'])]
     public function like(Idea $idea, LikeRepository $likeRepository): Response
     {
         $like = new Like();
@@ -27,6 +27,32 @@ class LikeController extends AbstractController
 
     #[Route('/idea/{id}/dislike', name: 'app_like_dislike', methods: ['GET', 'POST'])]
     public function dislike(Idea $idea, LikeRepository $likeRepository): Response
+    {
+        $ideaUser = $likeRepository->findOneBy(['user' => $this->getUser(), 'idea' => $idea->getId()]);
+        $ideaViews = $idea->getIdeaViews();
+        $idea->setIdeaViews($ideaViews - 1);
+        $likeRepository->remove($ideaUser, true);
+        return $this->redirectToRoute('app_idea_show', [
+            'id' => $idea->getId(),
+        ], Response::HTTP_SEE_OTHER);
+    }
+
+    #[Route('///idea/{id}/{orderBy}/like', name: 'app_like_like_comment', methods: ['GET', 'POST'])]
+    public function likeFromComment(Idea $idea, LikeRepository $likeRepository): Response
+    {
+        $like = new Like();
+        $like->setIdea($idea);
+        $like->setUser($this->getUser());
+        $ideaViews = $idea->getIdeaViews();
+        $idea->setIdeaViews($ideaViews - 1);
+        $likeRepository->save($like, true);
+        return $this->redirectToRoute('app_idea_show', [
+            'id' => $idea->getId(),
+        ], Response::HTTP_SEE_OTHER);
+    }
+
+    #[Route('//idea/{id}/{orderBy}/dislike', name: 'app_like_dislike_comment', methods: ['GET', 'POST'])]
+    public function dislikeFromComment(Idea $idea, LikeRepository $likeRepository): Response
     {
         $ideaUser = $likeRepository->findOneBy(['user' => $this->getUser(), 'idea' => $idea->getId()]);
         $ideaViews = $idea->getIdeaViews();
