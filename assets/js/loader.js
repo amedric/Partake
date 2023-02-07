c9dfbd727008510a72d54abc702120f53164c74f
amedric <andyricana@hotmail.com>
2023-02-07 22:27:19 +0100

    commit changes

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -1,11 +1,12 @@
 const preloader = document.querySelector('.ptk-loaderContainer');
-if (preloader) {
-    window.addEventListener('load', () => {
-        setTimeout(() => {
-            preloader.classList.add('loaded');
+// if (preloader) {
+window.addEventListener('load', () => {
+    console.log('Loading')
+    setTimeout(() => {
+        preloader.classList.add('loader-hidden');
         }, 1000);
         setTimeout(() => {
             preloader.remove();
         }, 2000);
     });
-}
+// }
